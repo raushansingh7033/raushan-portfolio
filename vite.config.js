198a1c87594af7f0e2928f594fef0d01efe5c8f1
Raushan Singh <raushan786267@gmail.com>
2026-01-28 18:21:55 +0530

    some updated

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/raushan-singh/",
-  // server: {
-  //   host: "0.0.0.0",
-  //   port: 5173,
-  // }
+  base: "/raushan-portfolio/",  // <--- MATCH YOUR GITHUB PAGES REPO NAME
 });
